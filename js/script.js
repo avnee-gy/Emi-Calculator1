4d4933d471c3eabe229f6a17f9b26834a019708e
avnee-gy <avneegoyal410@gmail.com>
2023-04-04 22:31:33 +0530

    logic changed

--- js/script.js
+++ js/script.js
@@ -16,15 +16,16 @@ loanTenure.oninput = (()=>{
         out2.classList.remove("show");
     });
 
-    loanAmount.oninput = (()=>{
+loanAmount.oninput = (()=>{
     let value = loanAmount.value;
     out1.textContent = value;
     out1.style.left = (value/2) + "%";
     out1.classList.add("show");
-    });
-    loanAmount.onblur = (()=>{
-        out1.classList.remove("show");
-    });
+    }
+);
+loanAmount.onblur = (()=>{
+    out1.classList.remove("show");
+});
 
 
 
@@ -41,20 +42,34 @@ const submitBtn = document.querySelector("#calc");
     
     submitBtn.addEventListener("click", function(){
     
+        // amount = loanAmount.value;
+        // tenure = (loanTenure.value); // 1Year = 12 months
+        // let time = tenure/12;
+        // rate = (loanRate.value); // loan rate per year / 100 = loan percentage
+        // total= amount*(Math.pow(1+(rate/100),time)) //compound interest formula 
+        // emi = total/(tenure*12);
+        // emi = amount*rate*Math.pow((1+rate),tenure)/(Math.pow((1+r), tenure)-1);
+        // // total = emi * tenure; // total amount to be paid including interest
+        // interest = total - amount // interest = total amount - principle amount
+
+
         amount = loanAmount.value;
         tenure = (loanTenure.value); // 1Year = 12 months
-        let time = tenure/12;
+        time = tenure/12;
         rate = (loanRate.value); // loan rate per year / 100 = loan percentage
-        total= amount*(Math.pow(1+(rate/100),time)) //compound interest formula 
-        emi = amount*rate* (1+rate)/((1+rate)*tenure -1);
-        // total = emi * tenure; // total amount to be paid including interest
-        interest = total - amount // interest = total amount - principle amount
+        total= amount*(Math.pow(1+(rate/100),time)) ;//compound interest formula 
+        // emi = amount*rate*Math.pow((1+rate),tenure*12)/(Math.pow((1+r), tenure*12)-1);
+        monthly_rate = rate/(12*100);
+        frst_int = monthly_rate*amount;
+        emi = Math.pow((1+ monthly_rate), (tenure-1)) / (Math.pow((1+ monthly_rate), (tenure-1)) -1) * frst_int;
+    // total = emi * tenure; // total amount to be paid including interest
+        interest = total - amount ;
        
     
         loanEmi.innerHTML = Math.floor(emi);
         // loanPrinciple.innerHTML = Math.floor(amount);
-        loanTotal.innerHTML = Math.floor(total);
-        loanInterest.innerHTML = Math.floor(interest);
+        loanTotal.innerHTML = Math.floor (total);
+        loanInterest.innerHTML = Math.floor (interest);
     
     
         
